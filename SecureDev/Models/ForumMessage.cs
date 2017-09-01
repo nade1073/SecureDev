6a30e023e3b2ece7785334c53be58849d91284f4
nade1073 <nade1073@gmail.com>
2017-09-01 14:33:36 +0300

    Create topic added to Forum

--- SecureDev/Models/ForumMessage.cs
+++ SecureDev/Models/ForumMessage.cs
@@ -10,13 +10,15 @@ namespace Vladi2.Models
         public string SubjectMessage { get; set; }
         public string TopicMessage { get; set; }
         public string Message { get; set; }
+        public string UserName { get; set;}
 
         public override string ToString()
         {
             return string.Format(
 @"SubjectMessage {0}
 TopicMessage {1}
-Message {2}", SubjectMessage, TopicMessage, Message);
+Message {2}
+UseName {3}", SubjectMessage, TopicMessage, Message, UserName);
         }
 
 
