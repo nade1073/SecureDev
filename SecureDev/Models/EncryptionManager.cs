ef9b7768e551a10a8c797088c4637a8fa804437b
nade1073 <nade1073@gmail.com>
2017-10-14 15:42:56 +0300

    Last cahnge Salt password

--- SecureDev/Models/EncryptionManager.cs
+++ SecureDev/Models/EncryptionManager.cs
@@ -10,12 +10,13 @@ namespace Vladi2.Models
 {
     public static class EncryptionManager
     {
-        private  static string salt = "Ben&Netanel&Nadav";
+        private  static String salt = "Ben&Netanel&Nadav";
         public static String getSHA256Password(String password)
         {
+            String newPasswordwithsalt = password + salt;
             SHA256 sha256 = SHA256Managed.Create();
             var retPassword = String.Empty;
-            byte[] cryptPassword = sha256.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
+            byte[] cryptPassword = sha256.ComputeHash(Encoding.ASCII.GetBytes(newPasswordwithsalt), 0, Encoding.ASCII.GetByteCount(newPasswordwithsalt));
             foreach (byte theByte in cryptPassword)
             {
                 retPassword += theByte.ToString("x2");
